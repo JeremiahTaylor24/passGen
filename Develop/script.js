50d34e17671312882840aa9a4be7e56334224b2d
Jeremiah Taylor <jt84air@gmail.com>
2024-01-17 21:31:43 -0600

    just get it in for now

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,61 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+
+
+const length = 14;
+const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChars = "0123456789";
+const symbolChars = "!@#$%^&*()?";
+const allChars = lowerCaseChars + upperCaseChars + numberChars + symbolChars;
+
+
+    function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
+
+      
+    
+      
+      let password = '';
+      password += upperCaseChars[Math.floor(Math.random()* upperCaseChars.length)];
+      password += lowerCaseChars[Math.floor(Math.random()* lowerCaseChars.length)];
+      password += numberChars[Math.floor(Math.random()* numberChars.length)];
+      password += symbolChars[Math.floor(Math.random()* symbolChars.length)];
+
+      while(length > password.length){
+
+        password += allChars[Math.floor(Math.random()* allChars.length)];
+
+      }
+
+      
+
+
+
+    
+     
+    
+      return password;
+}
+  
+    
+  
+    
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
